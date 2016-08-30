108aa429642e58fba4d2e7ed63dd767368dcb0e2
Michael Rutz <michael.rutz@sclable.com>
2016-08-30 11:12:48 +0200

    fix clear compiled command Inject SmartyFactory directly to the fire method, thus smarty is configured and ready to clear the compiled templates.

--- src/Console/ClearCompiledCommand.php
+++ src/Console/ClearCompiledCommand.php
@@ -17,9 +17,9 @@
  */
 namespace Ytake\LaravelSmarty\Console;
 
-use Smarty;
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
+use Ytake\LaravelSmarty\SmartyFactory;
 
 /**
  * Class ClearCompiledCommand
@@ -29,18 +29,6 @@ use Symfony\Component\Console\Input\InputOption;
  */
 class ClearCompiledCommand extends Command
 {
-    /** @var Smarty */
-    protected $smarty;
-
-    /**
-     * @param Smarty $smarty
-     */
-    public function __construct(Smarty $smarty)
-    {
-        parent::__construct();
-        $this->smarty = $smarty;
-    }
-
     /**
      * The console command name.
      *
@@ -58,17 +46,16 @@ class ClearCompiledCommand extends Command
     /**
      * Execute the console command.
      *
-     * @return void
+     * @param \Ytake\LaravelSmarty\SmartyFactory $smartyFactory
+     * @return int
      */
-    public function fire()
+    public function fire(SmartyFactory $smartyFactory)
     {
-        $smartyExtension = $this->smarty->ext;
-        $class = $smartyExtension->clearCompiledTemplate;
-        if ($class->clearCompiledTemplate($this->smarty, $this->option('file'), $this->option('compile_id'))) {
-            $this->info('done.');
-            return;
-        }
-        return;
+        $removedFiles = $smartyFactory->getSmarty()
+            ->clearCompiledTemplate($this->option('file'), $this->option('compile_id'));
+
+        $this->info("removed $removedFiles files.");
+        return 0;
     }
 
     /**
